c90cd4d183ed60f9c543158babd39b2d255b051f
ShamyD <Seamus.D96@gmail.com>
2020-09-06 20:01:25 +0200

    Liten förändring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from spline import Spline
+from scipy import *
+from matplotlib.pyplot import *
 
 d = 2, 3, 4, 5
 s = Spline(d)
--- spline.py
+++ spline.py
@@ -1,7 +1,11 @@
-Class Spline:
+from scipy import *
+from matplotlib.pyplot import *
+
+class Spline:
 
     def __init__(self, d):
         self.d = d
+        self.us = linspace(0,1,10)
 
     def display(self):
         return self.d
