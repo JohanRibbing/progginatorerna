811c54c281c3f2d45a568968a5acbb9210b81ca5
JohanRibbing <ribbing.johan.1998@gmail.com>
2020-09-07 15:53:28 +0200

    implemented Spline.plot()

--- spline.py
+++ spline.py
@@ -1,11 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class Spline:
     
     def __init__(self, us, ds):
         self.us = us
         self.ds = ds
 
+    #antar att self.us är sorterad stigande och att ds är sorterad för att passa us
     def plot(self):
-        return
+        dx, dy = zip(*ds)
+        plt.plot(dx, dy, 'r--')
+        plt.plot(dx, dy, 'ro')
+
+        ss = [self.call(u) for u in self.us]
+        ssx, ssy = zip(*ss)
+        plt.plot(ssx, ssy, 'b+')
+
+        u_interval = np.linspace(us[0], us[-1], 100).tolist()
+        s_interval = [self.call(u) for u in u_interval]
+        s_interval_x, s_interval_y = zip(*s_interval)
+        plt.plot(s_interval_x, s_interval_y, 'b-')
+
+        plt.show()
+
 
     def call(self, u):
         return
@@ -25,6 +43,11 @@ class Spline:
         return 
         #return alpha
 
+print(111)
+us = np.linspace(0,1,4)
+ds = [(0,0), (1,0), (1,1), (0,1)]
 
-
- 
+larry = Spline(us, ds)
+print(222)
+larry.plot()
+print(333)
