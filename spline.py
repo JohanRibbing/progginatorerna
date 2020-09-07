d6e806663e7e2222977e9dac436346b20bf423b8
JohanRibbing <ribbing.johan.1998@gmail.com>
2020-09-07 12:22:23 +0200

    defined functions in spline.py

--- spline.py
+++ spline.py
@@ -1,4 +1,24 @@
-Class Spline
-    def testfunc(x):
-        return 2*x
+class Spline
     
+    def __init__(self, us, ds):
+        self.us = us
+        self.ds = ds
+
+    def plot():
+        return
+
+    def call(u):
+        return
+        #return (x,y)
+
+    def hot_index(u):
+        return
+        #return index
+
+    def alpha(u, n, k)
+        return 
+        #return alpha
+
+
+
+ 
