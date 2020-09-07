dc1dfdd7d2373f640c9b4021e5f7f62b1c28b361
ShamyD <Seamus.D96@gmail.com>
2020-09-07 17:12:27 +0200

    Implementerat alpha-funktionen

--- spline.py
+++ spline.py
@@ -3,22 +3,25 @@ from matplotlib.pyplot import *
 
 class Spline:
 
+
     def __init__(self, us, ds):
         self.us = us
         self.ds = ds
 
-    def plot():
+    def plot(self):
         return
 
-    def call(u):
+    def call(self,u):
         return
         #return (x,y)
 
-    def hot_index(u):
+    def hot_index(self,u):
         return
         #return index
 
-    def alpha(u, n, k)
-        return 
+    def alpha(self, u, n, k):
+        u_leftmost = self.us[k-1]
+        u_rightmost = self.us[k+3-n]
+        return ((u_rightmost-u)/(u_rightmost-u_leftmost))
         #return alpha
- 
+
