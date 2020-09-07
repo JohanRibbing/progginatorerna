a15a8d31f818144fe08558cab358417a9f3a6ce3
ShamyD <Seamus.D96@gmail.com>
2020-09-07 19:37:28 +0200

    Ändrat i main och Data

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from spline import Spline
 from scipy import *
 from matplotlib.pyplot import *
+import numpy
 
-d = 2, 3, 4, 5
-s = Spline(d)
-print(s.display())
+ds = 2, 3, 4, 5
+us = numpy.linspace(0,1,5)
+s = Spline(ds,us)
 
 #Något som läser in d:na från fil (tar in filnamn som string och returnerar en lista med tuples)
 #Instansierar splinen med listan med tuples
