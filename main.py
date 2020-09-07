e993724cc0c15a69abe151ac64b66de4c87ea227
ShamyD <Seamus.D96@gmail.com>
2020-09-07 21:34:00 +0200

    Skrivit en inläsningsrutin

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from spline import Spline
 from scipy import *
 from matplotlib.pyplot import *
 import numpy
+import csv
 
-ds = 2, 3, 4, 5
-us = numpy.linspace(0,1,5)
-s = Spline(ds,us)
+def read_Data(filename):
+    d_s = []
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+               line_count +=1
+            else:
+                #[x = float(x) for x in row]
+                for i in range(len(row)):
+                    row[i] = float(row[i])
+                d_s.append(tuple(row))
+                line_count +=1
+    return d_s
+
+ds = read_Data('Data.csv')
+print(ds)
+#us = numpy.linspace(0,1,5)
+#s = Spline(ds,us)
 
 #Något som läser in d:na från fil (tar in filnamn som string och returnerar en lista med tuples)
 #Instansierar splinen med listan med tuples
