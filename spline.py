d1a3551bdaff60043848da6c61007b1224131b36
JohanRibbing <ribbing.johan.1998@gmail.com>
2020-09-08 10:08:39 +0200

    first try at implementing call, problems with endpoints

--- spline.py
+++ spline.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
 
 class Spline:
-
+    
+    #ds assumed to be list of ndarrays
     def __init__(self, us, ds):
         self.us = us
         self.ds = ds
@@ -14,21 +14,27 @@ class Spline:
         plt.plot(dx, dy, 'r--')
         plt.plot(dx, dy, 'ro')
 
-        ss = [self.call(u) for u in self.us]
+        ss = [self(u) for u in self.us]
         ssx, ssy = zip(*ss)
         plt.plot(ssx, ssy, 'b+')
 
         u_interval = np.linspace(self.us[0], self.us[-1], 100).tolist()
-        s_interval = [self.call(u) for u in u_interval]
+        s_interval = [self(u) for u in u_interval]
         s_interval_x, s_interval_y = zip(*s_interval)
         plt.plot(s_interval_x, s_interval_y, 'b-')
 
         plt.show()
+    
+    def d(self, n, k, u):
+        if n == 0:
+            return self.ds[k]
+        elif n >= 1:
+            return self.alpha(n, k, u) * self.d(n-1, k-1, u) + (1-self.alpha(n, k, u)) * self.d(n-1, k, u)
 
-
-    def call(self, u):
-        return
-        #return (x,y)
+    #need to figure out endpoints, multiplicity and index out of bounds
+    def __call__(self, u):
+        index = self.hot_index(u)
+        return self.d(3, index, u)
 
     def hot_index(self, u):
         """
@@ -38,10 +44,27 @@ class Spline:
         """
         for i, gp in enumerate(self.us):
             if gp > u:
-                return i, gp
+                return i
 
-    def alpha(self, u, n, k):
+    def alpha(self, n, k, u):
         u_leftmost = self.us[k-1]
         u_rightmost = self.us[k+3-n]
         return ((u_rightmost-u)/(u_rightmost-u_leftmost))
-        #return alpha
+
+
+
+
+
+
+
+
+
+us = np.linspace(0,1,8)
+ds = [[0,0], [0,0], [0,0], [1,0], [1,1], [0,1], [0,1], [0,1]]
+ds = [np.array(d) for d in ds]
+larry = Spline(us, ds)
+larry.plot()
+
+
+
+
