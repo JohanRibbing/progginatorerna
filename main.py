c97e696336fa0cf06239a65c297cfcd730dbdc03
Elias Olson <elias973@hotmail.com>
2020-09-09 12:01:46 +0200

    Almost working basis repr of spline

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ us2 = create_Knot(ds2)
 #Creation of splines
 s1 = Spline(us1, ds1)
 s2 = Spline(us2, ds2)
+print(s2.spline_basis_representation(0.45))
 
 #Plot splines
-print(s1(0.2))
+# print(s1(0.2))
 s1.plot()
-print(s2(0.2))
+# print(s2(0.2))
 s2.plot()
