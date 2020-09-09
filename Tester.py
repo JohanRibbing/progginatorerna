8d57fa05512f710ee666e8beebe7855d053b042b
ShamyD <Seamus.D96@gmail.com>
2020-09-09 12:39:46 +0200

    created a unittest

--- Tester.py
+++ Tester.py
@@ -4,9 +4,13 @@ import unittest
 
 class TestIdentity(unittest.TestCase):
     def test(self):
-        ds = read_Data(Data.csv)
+        ds = read_Data("Data.csv")
         us = create_Knot(ds)
         s = Spline(us, ds)
-        #result =
-        #expected =
-        #self.assertAlmostEqual(result, expected)
+        result = 4
+        expected = 4
+        #s.__call__(0.3)
+        self.assertAlmostEqual(result, expected)
+
+if __name__=='__main__':
+    unittest.main()
